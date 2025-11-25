08a01237d774eb2d2209641cd079ebc463d02834
ArturoZamoranoB <arturozamoranobeltran9@gmail.com>
2025-11-25 03:36:57 -0700

    actuzalizaionde frontend10

--- db.js
+++ db.js
@@ -1,13 +1,10 @@
 const { Pool } = require("pg");
 
-let url = process.env.DATABASE_URL;
-
-// FORZAR IPv4 EN SUPABASE
-url = url.replace(".supabase.co", ".supabase.co.ipv4");
-
 const pool = new Pool({
-  connectionString: url,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
