2e84ab18c780367566f80eb446e4377c12957a6b
ArturoZamoranoB <arturozamoranobeltran9@gmail.com>
2025-11-25 03:54:13 -0700

    actuzalizaionde frontend11

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-const { Pool } = require("pg");
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false, // necesario para Neon en Render
+  },
+  max: 5,                       // recomendado para PostgreSQL serverless
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
-
-module.exports = pool;
